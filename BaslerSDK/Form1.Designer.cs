568fd089592b038a84548112925283350c03e5f8
宏伟 谢 <1449952434@qq.com>
2021-07-17 22:41:46 +0800

    添加项目文件。

---
+++ BaslerSDK/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace BaslerSDK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.button_Oneshot = new System.Windows.Forms.Button();
+            this.button_keepShot = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(336, 339);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(354, 13);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(118, 35);
+            this.button_stop.TabIndex = 1;
+            this.button_stop.Text = "button_stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // button_Oneshot
+            // 
+            this.button_Oneshot.Location = new System.Drawing.Point(354, 54);
+            this.button_Oneshot.Name = "button_Oneshot";
+            this.button_Oneshot.Size = new System.Drawing.Size(118, 35);
+            this.button_Oneshot.TabIndex = 2;
+            this.button_Oneshot.Text = "button_Oneshot";
+            this.button_Oneshot.UseVisualStyleBackColor = true;
+            this.button_Oneshot.Click += new System.EventHandler(this.button_Oneshot_Click);
+            // 
+            // button_keepShot
+            // 
+            this.button_keepShot.Location = new System.Drawing.Point(354, 96);
+            this.button_keepShot.Name = "button_keepShot";
+            this.button_keepShot.Size = new System.Drawing.Size(118, 40);
+            this.button_keepShot.TabIndex = 3;
+            this.button_keepShot.Text = "button_keepShot";
+            this.button_keepShot.UseVisualStyleBackColor = true;
+            this.button_keepShot.Click += new System.EventHandler(this.button_keepShot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_keepShot);
+            this.Controls.Add(this.button_Oneshot);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Button button_Oneshot;
+        private System.Windows.Forms.Button button_keepShot;
+    }
+}
+
---
+++ BaslerSDK/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Basler.Pylon;
+using System.Drawing.Imaging;
+
+namespace BaslerSDK
+{
+
+    public partial class Form1 : Form
+    {
+        baslerCamcs camera = new baslerCamcs();
+        public Form1()
+        {
+            InitializeComponent();
+            camera.CameraImageEvent += Camera_CameraImageEvent;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Camera_CameraImageEvent(Bitmap bmp)
+        {
+            pictureBox1.Invoke(new MethodInvoker(delegate
+            {
+            Bitmap old = pictureBox1.Image as Bitmap;
+            pictureBox1.Image = bmp;
+            if (old != null)
+                old.Dispose();
+            }));
+        }
+        void Unanble()
+        {
+            button_stop.Enabled = false;
+            button_Oneshot.Enabled = false;
+            button_keepShot.Enabled = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (camera.CameraNumber > 0)
+                camera.CameraInit();
+            else
+            {
+                MessageBox.Show("未连接到相机");
+                Unanble();
+            }
+
+        }
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            camera.Stop();
+        }
+
+        private void button_Oneshot_Click(object sender, EventArgs e)
+        {
+            camera.OneShot();
+        }
+
+        private void button_keepShot_Click(object sender, EventArgs e)
+        {
+            camera.KeepShot();
+        }
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            camera.DestroyCamera();
+        }
+
+
+    }
+
+
+
+
+}
